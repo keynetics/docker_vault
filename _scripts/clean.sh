23eb64be7ecd9375aaeb4f5f57fedcb2fc381329
Noah Pritikin <noah.pritikin@kount.com>
2019-03-13 05:46:09 -0600

    added cleaning up of the secrets folder if it exists

--- _scripts/clean.sh
+++ _scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "[?] Are you sure you want to remove all Vault's data (y/n)? " answer
+read -r -p "[?] Are you sure you want to remove all Vault's data (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
         echo "[*] Removing files..."
@@ -9,6 +9,10 @@ case ${answer:0:1} in
 		rm -rf ./_data/backup/
 		echo "[+] Removing: ./_data/keys.txt"
 		rm -f ./_data/keys.txt
+        if [ -d "../secret-ids" ]; then
+            echo "[+] Removing: ../secret-ids/*"
+            rm -f ../secret-ids/*
+        fi
     ;;
     * )
         echo "[*] Aborting..."
