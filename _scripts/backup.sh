b2ecb7d8335f02136f84d686127e6ac226919678
Noah Pritikin <noah.pritikin@kount.com>
2019-03-13 06:41:05 -0600

    fixed backup script

--- _scripts/backup.sh
+++ _scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "[*] Executing backup..."
-docker-compose run backup consul-backup -i consul:8500 -t "$(grep 'backup_token:' ./_data/keys.txt | awk -v RS='\r\n' '{printf $2}')  backup_$(date +%Y-%m-%d)"
+docker-compose run backup consul-backup -i consul:8500 -t "$(grep 'backup_token:' ./_data/keys.txt | awk -v RS='\r\n' '{printf $2}' | perl -pe 's/\x1b\[[0-9;]*[a-zA-Z]//g')"  backup_$(date +%Y-%m-%d)
